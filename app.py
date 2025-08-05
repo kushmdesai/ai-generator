ae5f8bd46f3074c29d40b4872279266923da05f7
kush <kushmdesai@icloud.com>
2025-08-05 12:20:28 -0400

    finished short-story generate and now working on image generator

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from PIL import Image
+from io import BytesIO
+import base64
 
 # Configure the client with your API key
 # Make sure to set the GEMINI_API_KEY environment variable
@@ -13,12 +16,23 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 app = Flask(__name__)
 
 try:
-    with open('system_instruction.txt','r') as file:
-        system_instruction = file.read()
+    with open('./system_instruction/essay.txt','r') as file:
+        essayTxt = file.read()
+        print("found essay.txt")
 except FileNotFoundError:
-    print("Error: system_instruction.txt not found")
+    print("Error: essay.txt not found")
 except Exception as e:
     print(f"an error occured: {e}")
+
+try:
+    with open('./system_instruction/short-story.txt','r') as file:
+        shortStoryTxt = file.read()
+        print("found short-story.txt")
+except FileNotFoundError:
+    print("Error: short-story.txt not found")
+except Exception as e:
+    print(f"an error occured: {e}")
+
 
 @app.route("/")
 def home():
@@ -32,7 +46,7 @@ def essay_writer():
         response = client.models.generate_content(
             model='gemini-2.5-pro',
             config= types.GenerateContentConfig(
-                system_instruction=system_instruction
+                system_instruction=essayTxt
             ),
             contents=f"Write an essay about {topic} with a length of approximately {length} words."
         )    
@@ -41,13 +55,38 @@ def essay_writer():
 
     return render_template('essay-writer.html')
 
-@app.route("/short-story")
+@app.route("/short-story", methods=['GET','POST'])
 def short_story():
-    return render_template('placeholder.html', page_name='Short Story Writer')
+    if request.method == 'POST':
+        topic = request.form.get('topic')
+        response = client.models.generate_content(
+            model='gemini-2.5-pro',
+            config= types.GenerateContentConfig(
+                system_instruction=shortStoryTxt
+            ),
+            contents=f"Write a short story about {topic} that is interesting and compelling to read."
+        )    
+        story = response.text
+        return render_template('short-story.html', story=story)
+
+    return render_template('short-story.html')
 
 @app.route("/image-generation")
 def image_generation():
-    return render_template('placeholder.html', page_name='Image Generator')
+    if request.method == 'POST':
+        topic = request.form.get('topic')
+        response = client.models.generate_content(
+            model='gemini-2.5-pro',
+            config= types.GenerateContentConfig(
+                system_instruction=shortStoryTxt
+            ),
+            contents=f"Write a short story about {topic} that is interesting and compelling to read."
+        )    
+        story = response.text
+        return render_template('short-story.html', story=story)
+
+    return render_template('short-story.html')
+    
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
