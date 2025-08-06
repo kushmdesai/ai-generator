ce96f9df1311a71bacabadb9ae0c6baca924a506
kush <kushmdesai@icloud.com>
2025-08-06 15:22:02 -0400

    finally speech to text works. I may do image to text but I wil think about it not sure yet.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from google.genai import types
 from google.genai.errors import ClientError
 from dotenv import load_dotenv
 from PIL import Image
-from io import BytesIO
+from werkzeug.utils import secure_filename
 
 # Configure the client with your API key
 # Make sure to set the GEMINI_API_KEY environment variable
@@ -21,68 +21,107 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
    with wave.open(filename, "wb") as wf:
+      
       wf.setnchannels(channels)
       wf.setsampwidth(sample_width)
       wf.setframerate(rate)
       wf.writeframes(pcm)
 
+
+try:
+    with open('./system_instruction/code-gen.txt','r') as file:
+
+        codeGenTxt = file.read()
+        print("found code-gen.txt")
+
+except FileNotFoundError:
+    print("Error: code-gen.txt not found")
+
+except Exception as e:
+    print(f"an error occured: {e}")
+
 try:
     with open('./system_instruction/essay.txt','r') as file:
+
         essayTxt = file.read()
         print("found essay.txt")
+
 except FileNotFoundError:
     print("Error: essay.txt not found")
+
 except Exception as e:
     print(f"an error occured: {e}")
 
 try:
     with open('./system_instruction/short-story.txt','r') as file:
+
         shortStoryTxt = file.read()
         print("found short-story.txt")
+
 except FileNotFoundError:
     print("Error: short-story.txt not found")
+
 except Exception as e:
     print(f"an error occured: {e}")
 
 try:
+
     with open('./system_instruction/image-gen.txt','r') as file:
+
         imageGenTxt = file.read()
         print("found image-gen.txt")
+
 except FileNotFoundError:
+
     print("Error: image-gen.txt not found")
+
 except Exception as e:
+
     print(f"an error occured: {e}")
 
 @app.route("/")
 def home():
+
     return render_template('home.html')
 
 @app.route("/essay-writer", methods=['GET', 'POST'])
 def essay_writer():
+
     if request.method == 'POST':
+
         topic = request.form.get('topic')
         length = request.form.get('length')
+
         response = client.models.generate_content(
+
             model='gemini-2.5-pro',
             config= types.GenerateContentConfig(
+
                 system_instruction=essayTxt
             ),
+
             contents=f"Write an essay about {topic} with a length of approximately {length} words."
         )    
         essay = response.text
+
         return render_template('essay-writer.html', essay=essay)
 
     return render_template('essay-writer.html')
 
 @app.route("/short-story", methods=['GET','POST'])
 def short_story():
+
     if request.method == 'POST':
+
         topic = request.form.get('topic')
         response = client.models.generate_content(
+
             model='gemini-2.5-pro',
             config= types.GenerateContentConfig(
+
                 system_instruction=shortStoryTxt
             ),
+
             contents=f"Write a short story about {topic} that is interesting and compelling to read."
         )    
         story = response.text
@@ -92,12 +131,17 @@ def short_story():
 
 @app.route("/image-generation", methods =['GET','POST'])
 def image_generation():
+
     if request.method == 'POST':
+
         topic = request.form.get('topic')
+
         response = client.models.generate_content(
+
            model="gemini-2.0-flash-preview-image-generation",
            contents=f"Draw an image about {topic}.",
-           config= types.GenerateContentConfig(
+
+           config= types.GenerateContentConfig(    
                response_modalities=['TEXT','IMAGE'],
            )
         )
@@ -142,75 +186,95 @@ def image_generation():
 
 #     return render_template('video_gen.html')
 
-try:
-    with open('./system_instruction/code-gen.txt','r') as file:
-        codeGenTxt = file.read()
-        print("found code-gen.txt")
-except FileNotFoundError:
-    print("Error: code-gen.txt not found")
-except Exception as e:
-    print(f"an error occured: {e}")
-
 @app.route("/code-generation", methods=['GET', 'POST'])
 def code_generation():
+
     if request.method == 'POST':
+
         topic = request.form.get('topic')
+
         response = client.models.generate_content(
+
             model='gemini-2.5-pro',
             config=types.GenerateContentConfig(
+
                 system_instruction=codeGenTxt
             ),
             contents=f"Write code for {topic}."
         )
         code = markdown.markdown(response.text, extensions=['fenced_code'])
+
         return render_template('code_gen.html', code=code)
 
     return render_template('code_gen.html')
 
 @app.route("/chatbot", methods = ['GET','POST'])
 def chatbot():
+
     if 'chat_history' not in session:
         session['chat_history'] = []
+
     bot_response = None
+
     if request.method == 'POST':
+
         user_input = request.form.get('user_input', '').strip()
+
         if user_input:
+
             try:
+
                 response = client.models.generate_content(
                     model='gemini-2.5-pro',
                     contents=user_input
                 )
+
                 bot_response = response.text
 
                 session['chat_history'].append({
                     'user': user_input,
                     'bot' : bot_response
                 })
+
                 session.modified = True
                 
             except ClientError as e:
+                
                 if "RESOURCE_EXHAUSTED":
                     bot_response = 'QUOTA EXCEEDED! Try again tomorrow or upgrade your plan!'
+
                 else:
                     bot_response = f'An error occured: {e}'
+
             except Exception as e:
                 bot_response = f' Unexpected error: {e}'
+
     return render_template('chatbot.html', chat_history = session['chat_history'])
 
 @app.route("/text-to-speech", methods=['GET', 'POST'])
 def text_to_speech():
+
     audio_path = None
+
     if request.method == 'POST':
+
         text = request.form.get('text', '').strip()
+
         try:
             response = client.models.generate_content(
+
                 model="gemini-2.5-flash-preview-tts",
                 contents=text,
+
                 config=types.GenerateContentConfig(
+
                     response_modalities=["AUDIO"],
                     speech_config=types.SpeechConfig(
+
                         voice_config=types.VoiceConfig(
+
                             prebuilt_voice_config=types.PrebuiltVoiceConfig(
+
                             voice_name='Kore',
                             )
                         )
@@ -218,16 +282,24 @@ def text_to_speech():
                 )
             )
         except ClientError as e:
+
             if "RESOURCE_EXHAUSTED" in str(e):
+
                 print("⚠️ Quota exceeded! Try again tomorrow or upgrade your plan.")
+
         output_folder = os.path.join(app.root_path, 'static', 'generated')
         os.makedirs(output_folder, exist_ok=True)
+
         timestamp = int(time.time() * 1000)
+
         data = response.candidates[0].content.parts[0].inline_data.data
         file_name=f'tts_{timestamp}.wav'
+
         fullpath = os.path.join(output_folder, file_name)
         wave_file(fullpath, data)
+
         audio_path = f'/generated/{file_name}'
+
         return render_template('text_to_speech.html', audio=audio_path)
     
     # For GET requests
@@ -236,7 +308,32 @@ def text_to_speech():
 
 @app.route("/speech-to-text", methods = ['GET','POST'])
 def speech_to_text():
-    return render_template('placeholder.html', page_name='Speech to Text')
+    UPLOAD_FOLDER = 'static/uploads'
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+    if request.method == 'POST':
+
+        file = request.files.get('audio_file')
+
+        if not file:
+            return 'no file uploaded', 400
+        
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(filepath)
+
+        myfile = client.files.upload(file=filepath)
+        prompt = 'Transcibe the audio file'
+
+        response = client.models.generate_content(
+            model = 'gemini-2.5-flash',
+            contents=[prompt, myfile]       
+        )  
+
+        text = response.text
+        return render_template('speech_to_text.html', text=text)
+    
+    return render_template('speech_to_text.html')
     
 
 if __name__ == '__main__':
